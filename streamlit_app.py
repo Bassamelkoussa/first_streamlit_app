8ce40244611f0dfcd9f94515b5418aa4496fb517
Bassam El Koussa <95539007+Bassamelkoussa@users.noreply.github.com>
2023-06-23 09:22:41 +0100

    Update streamlit_app.py
    lock display on pre-selected items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+#Landing header
 import streamlit
 streamlit.title("My Mom's New Healthy Dinner")
 streamlit.header('Breakfast Favorites')
@@ -7,12 +8,14 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+#Import library and table
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
 #display table on webpage
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
